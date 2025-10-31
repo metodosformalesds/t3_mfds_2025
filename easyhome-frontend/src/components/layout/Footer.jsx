00ae4b05a09698bc8b7005dd7fdb2f3411ccc6a3
Cristian <cristian.martinez.716cm9@gmail.com>
2025-10-31 12:33:45 -0600

    feat: add Footer component for application layout

---
+++ easyhome-frontend/src/components/layout/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="app-footer">
+      <p>EasyHome &copy; 2025 - Sistema de Gestión Inmobiliaria</p>
+    </footer>
+  )
+}
+
+export default Footer
