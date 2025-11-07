36d9109249249b03bc0a63fb58d739624ac1f344
Sebastian Valencia Terrazas <al222929@alumnos.uacj.mx>
2025-11-06 18:58:58 -0700

    Creacion de componente para publicación.

---
+++ easyhome-frontend/src/components/features/Publicaciones.jsx
@@ -0,0 +1,46 @@
+import '../../assets/styles/publicaciones.css';
+
+
+function ServiceCard() {
+    return (
+        <section className="service-listings-section">
+            <h2 className="section-title">Servicios disponibles</h2>
+            
+            <div className="service-card">
+                
+                {/* Cabecera (Contenedor de Premium) */}
+                <div className="card-header">
+                    {/* Contenido principal */}
+                    <div className="provider-info">
+                        <img src="ruta/a/imagen_perfil.jpg" alt="Foto de perfil" className="profile-img" />
+                        <span className="provider-name"></span> {/* Nombre */}
+                    </div>
+
+                    {/* Etiqueta Premium */}
+                    <span className="premium-tag">Premium</span>
+                </div>
+
+                {/*Resumen y Tags*/}
+                <div className="card-body">
+                    <p className="description-text"></p>
+                    <div className="tags-container">
+                        
+                    </div>
+                </div>
+
+                {/*  Imágenes */}
+                <div className="gallery-container">
+                    {/*<img src="ruta/a/imagen_jardin_1.jpg" alt="Muestra de trabajo 1" className="work-image" />*/}
+                    
+                    
+                    {/* Ir al perfil*/}
+                    <a href="#" className="profile-button">Ir al perfil</a>
+                </div>
+
+                {/* Rango de precio */}
+                <p className="price-range"></p>
+            </div>
+        </section>
+    );
+}
+export default ServiceCard;
---
+++ easyhome-frontend/src/pages/cliente/feed.jsx
@@ -0,0 +1,20 @@
+function Feed() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Publicaciones</h1>
+      <p>Ponte al día con las actualizaciones más recientes de los profesionales de EayHome..</p>
+      <div style={{
+        marginTop: '20px',
+        padding: '10px',
+        backgroundColor: '#d1ecf1',
+        border: '1px solid #bee5eb',
+        borderRadius: '4px'
+      }}>
+
+        <h1> Servicios disponibles </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Feed;
