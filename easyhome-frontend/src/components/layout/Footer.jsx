2b819bb7844521d6c098ab9935e030bfac174e2c
JenniferVelo <jennifervelo2004@gmail.com>
2025-11-06 19:39:13 -0700

    feat: enhance footer with authentication links and styles

--- easyhome-frontend/src/components/layout/Footer.jsx
+++ easyhome-frontend/src/components/layout/Footer.jsx
@@ -1,7 +1,22 @@
 import '../../assets/styles/Footer.css'
+import { useAuth } from "react-oidc-context";
+import { Link } from 'react-router-dom';
 
 function Footer() {
+  const auth = useAuth();
+
+  const handleLogin = () => {
+    auth.signinRedirect();
+  };
+  const handleRegister = () => {
+    // URL completa de registro de Cognito
+    const cognitoSignupUrl = 'https://us-east-1gbsgbtrls.auth.us-east-1.amazoncognito.com/signup?client_id=478qnp7vk39jamq13sl8k4sp7t&code_challenge=8MNgRVeZQ4m-jnJljG70KrrwnX2liL7h3m433vYbEH0&code_challenge_method=S256&redirect_uri=http%3A%2F%2Flocalhost%3A5173&response_type=code&scope=email+openid+phone&state=95b01a30d90a47cd8d3e72eb405199b5';
+    // Redirige el navegador a la URL de registro
+    window.location.href = cognitoSignupUrl;
+    };
+
   return (
+    
     <footer className="app-footer">
       <div className="contenido-footer">
         <div className = "contacto">
@@ -28,9 +43,17 @@ function Footer() {
         <div className="mi-cuenta">
             <h3>Mi cuenta</h3>
             <ul>
-              <li>Registrarse</li>
-              <li>Iniciar Sesión</li>
-              <li>Ver planes</li>
+              <li>
+                <a href="#" onClick={(e) => { e.preventDefault(); handleRegister(); }}>
+                  Registrarse
+                </a>
+              </li>
+              <li>
+                <a href="#" onClick={(e) => { e.preventDefault(); handleLogin(); }}>
+                  Iniciar Sesión
+                </a>
+              </li>
+              <li><Link to="/subscriptions">Ver planes</Link></li>
             </ul>
         </div>
       </div>
