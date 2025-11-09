a0824e2c7572ff5e1dae01f923b09ef1147653ba
BrandonGus <al192880@alumnos.uacj.mx>
2025-11-09 13:21:05 -0700

    feat: add Pydantic schemas for subscription data validation

---
+++ easyhome-backend/app/schemas/schemas_suscripcion.py
@@ -0,0 +1,9 @@
+# app/schemas/schemas_suscripcion.py
+# ────────────────────────────────────────────────
+# Esquemas: Suscripciones
+# Referencia: RF-21 al RF-23, RF-34
+# Descripción:
+#   Define los modelos Pydantic utilizados para validar los datos
+#   de entrada y salida relacionados con las suscripciones y pagos.
+# ────────────────────────────────────────────────
+
