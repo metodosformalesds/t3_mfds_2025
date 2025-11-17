0968f23c478526879c9c5e7d88d3b2fe7165d1eb
JenniferVeloD <al222745@alumnos.uacj.mx>
2025-11-17 01:19:20 -0700

    Crear componente base ServiceFinishedAlert

--- easyhome-frontend/src/pages/cliente/alerta_finalizacion.jsx
+++ easyhome-frontend/src/pages/cliente/alerta_finalizacion.jsx
@@ -1,43 +1,15 @@
 import React from "react";
-import "../../assets/styles/service_finished_alert.css";
-import { FaStar } from "react-icons/fa";
+import "../../assets/styles/alerta_finalizacion.css";
+
+export default function ServiceFinishedAlert({ isOpen, alert }) {
+
+  if (!isOpen || !alert) return null;
 
   return (
     <div className="sf-alert-backdrop">
       <div className="sf-alert-container">
-
-        {/*Boton para cerrar */}
-        <button className="sf-alert-close" onClick={onClose}>×</button>
-
-        <h2 className="sf-alert-title">
-          {provider.nombreCompleto} ha finalizado un servicio contigo
-        </h2>
-
-        {/* Tarjeta del proveedor */}
-        <div className="sf-alert-provider-card">
-          <img
-            className="sf-alert-avatar"
-            src={provider.fotoPerfil || "/images/default-worker.jpg"}
-            alt="Foto del proveedor"
-          />
-
-          <div className="sf-alert-provider-info">
-            <p className="sf-provider-name">{provider.nombreCompleto}</p>
-            <p className="sf-provider-badge">Miembro premium de Easyhome</p>
-            <p className="sf-provider-rating">
-              <FaStar className="sf-star-icon" />{" "}
-              {provider.calificacionPromedio} ({provider.totalResenas})
-            </p>
-          </div>
-        </div>
-        <p className="sf-alert-help-text">Ayúdanos a mejorar tu experiencia</p>
-
-        {/* Botón para agregar reseña */}
-        <button className="sf-alert-btn-review" onClick={onReview}>
-          Agregar reseña
-        </button>
-
+        <h2 className="sf-alert-title">{alert.mensaje}</h2>
       </div>
     </div>
   );
-
+}
